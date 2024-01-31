240b81c09c9a565bf373379304f40319a592dd1d
gpresent <gpresent@tamu.edu>
2024-01-31 14:08:22 -0600

    Found a little demo of gestures in React Native

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import GestureDemo from './GestureDemo';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+      
+    //   <StatusBar style="auto" />
+    // </View>
+    <GestureDemo />
   );
 }
 
