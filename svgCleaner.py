f987a98cf232086fd6bfe744ce75bb88e317f227
Cole Broberg <gpresent@tamu.edu>
2024-02-01 20:12:22 -0600

    Proper cleaned kanji and script

--- svgCleaner.py
+++ svgCleaner.py
@@ -12,7 +12,7 @@ for file in os.listdir('./kanji'):
         with open(newdir + '/' + file, 'w', encoding='utf-8') as f2:
             for line in content.split('\n'):
                 if '<g id="kvg:StrokeNumbers' in line:
-                    f2.write('<g id="kvg:StrokeNumbers_' + file[0,file.find('.')] + '" font-size="8" fill="#210c0c"\n')
+                    f2.write('<g id=\"kvg:StrokeNumbers_' + file[0:file.find('.')] + '\" font-size=\"8\" fill=\"#210c0c\">\n')
                 else:
                     f2.write(line + '\n')
             print(str(filenum) + " files cleaned")
