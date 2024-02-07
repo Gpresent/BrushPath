bc1c26129ce67eb0470bcb49d17b6b82457e1646
gpresent <gpresent@tamu.edu>
2024-02-06 21:28:32 -0600

    Made joyo kanji imports more efficient

--- kanji_json_formation.py
+++ kanji_json_formation.py
@@ -1,14 +1,10 @@
 import os
 
 with open('joyo_kanji.tsx', 'w', encoding='utf-8') as kanji_tsx:
+    kanji_tsx.write("\nexport const kanjiSVGs: {[key: string]: any} = {")
     for file in os.listdir('./joyo_kanji'):
         unicode = file[:-4]
-        with open(f'./joyo_kanji/{file}', 'r', encoding='utf-8') as file:
-            kanji_tsx.write("const svg_" + unicode + " = require('./joyo_kanji/" + unicode + ".svg');\n")
-    kanji_tsx.write("\nexport const kanjiSVGs = {")
-    for file in os.listdir('./joyo_kanji'):
-        unicode = file[:-4]
-        kanji_tsx.write("\n    '" + unicode + "' : svg_" + unicode + ",")
+        kanji_tsx.write("\n    '" + unicode + "' : require('./joyo_kanji/" + unicode + ".svg'),")
     kanji_tsx.write("\n};")
     
             
