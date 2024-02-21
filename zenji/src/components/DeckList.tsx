488462600d28b0574f4e55352a6da9df2f06e0b3
Charlotte Harrington <c.harrington.0301@gmail.com>
2024-02-21 17:21:03 -0600

    repsonsive deck cards + list. more content on homepage

---
+++ zenji/src/components/DeckList.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import DeckCard from './DeckCard'; 
+import Deck from '../types/Deck';
+
+interface DeckListProps {
+  decks: Deck[];
+}
+
+const DeckList: React.FC<DeckListProps> = ({ decks }) => {
+  return (
+    <div className="deck-list">
+      {decks.map((deck) => (
+        <DeckCard key={deck.id} deck={deck} />
+      ))}
+    </div>
+  );
+};
+
+export default DeckList;
---
+++ zenji/src/components/HomeStudyPrompt.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "../styles.css";
+import Deck from "../types/Deck";
+
+interface HomeStudyPromptProps {
+  newUser: boolean;
+  suggestedDeck: Deck;
+}
+
+const HomeStudyPrompt: React.FC<HomeStudyPromptProps> = ({
+  newUser,
+  suggestedDeck,
+}) => {
+  return (
+    <div className="deck-prompt">
+      <div className="word-group">
+        {newUser ? (
+          <p className="continue-prompt">Start a new study session</p>
+        ) : (
+          <p className="continue-prompt">Continue your last study session</p>
+        )}
+        {newUser ? (
+          <p className="deck-name-prompt">Jump into {suggestedDeck.name}</p>
+        ) : (
+          <p className="deck-name-prompt">
+            You were learning {suggestedDeck.name}
+          </p>
+        )}
+      </div>
+      <div
+        className="image-wrapper"
+        style={{ backgroundImage: `url(${suggestedDeck.coverImage})` }}
+      >
+        <div />
+      </div>
+    </div>
+  );
+};
+
+export default HomeStudyPrompt;
