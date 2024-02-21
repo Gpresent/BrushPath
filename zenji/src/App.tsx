3b6574e4a86f201bc592878add0bfc18c321b48f
Charlotte Harrington <c.harrington.0301@gmail.com>
2024-02-21 09:21:58 -0600

    deck card component and deck type

--- zenji/src/App.tsx
+++ zenji/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react';
 import './App.css';
 import Draw from './pages/Draw';
+import DeckCard from './components/DeckCard';
 
 const styles = {
   container: {
@@ -12,11 +13,16 @@ const styles = {
     width: "100%",
   },
 };
-
+ 
 function App(this: any) {
   return (
     <div className="App" style={styles.container}>
       <Draw />
+      <DeckCard deck={{
+        id: 0,
+        coverImage: 'https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/dywxv8qwvzp-497%3A24178?alt=media&token=84fb6b9c-9a3a-4b4d-ab33-bbdbe677272b',
+        name: 'hi'
+      }} />
     </div>
   );
 }
