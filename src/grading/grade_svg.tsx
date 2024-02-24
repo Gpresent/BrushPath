bbf444bade6800fbe718f11c7c590ed8ff75ae8e
gpresent <gpresent@tamu.edu>
2024-02-24 00:45:36 -0600

    Working angle generation and interpolate upadtes to remove unneeded info

--- src/grading/grade_svg.tsx
+++ src/grading/grade_svg.tsx
@@ -3,16 +3,17 @@ import fit_bbox from './fit_bbox';
 import path_angles from './path_angles';
 
 export default function grade_svg(input: string, targetKanji: string) {
-        const svgInfo = interpolate(input);
-        const iCoords = svgInfo.coords;
         const targetInfoPromise = import("../interpolation_data/" + targetKanji.codePointAt(0)?.toString(16).padStart(5, '0') + ".json");
         targetInfoPromise.then((targetInfo) => {
             const tCoords = targetInfo.coords;
+            const iCoords = interpolate(input, targetInfo.totalLengths);
+            console.log("Input Coords: ", iCoords);
+            console.log("Target Coords: ", tCoords);
             const [inputCoords, targetCoords, aspectWarp] = fit_bbox(iCoords, tCoords);
             console.log("Tall boi: ", aspectWarp);
             console.log("Wide boi: ", 1 / aspectWarp);
-            //const [inputAngles, targetAngles] = path_angles(inputCoords, targetCoords);
-            //console.log("inputAngles: ", inputAngles);
-            //console.log("targetAngles: ", targetAngles);
+            const [inputAngles, targetAngles] = path_angles(inputCoords, targetCoords);
+            console.log("inputAngles: ", inputAngles);
+            console.log("targetAngles: ", targetAngles);
         });
 }
--- src/grading/path_angles.tsx
+++ src/grading/path_angles.tsx
@@ -4,14 +4,14 @@ export default function path_angles(inputCoords: number[][][], targetCoords: num
     var i, j;
     for (i = 0; i < inputCoords.length; i++) {
       inputAngles[i] = [];
-      for (j = 0; j < inputCoords[i].length; j++) {
-        inputAngles[i][j] = Math.atan2(inputCoords[i][j][1] - inputCoords[i][j][3], inputCoords[i][j][0] - inputCoords[i][j][2]);
+      for (j = 0; j < inputCoords[i].length - 1; j++) {
+        inputAngles[i][j] = Math.atan2(inputCoords[i][j + 1][1] - inputCoords[i][j][1], inputCoords[i][j + 1][0] - inputCoords[i][j][0]) * 180 / Math.PI;
       }
     }
     for (i = 0; i < targetCoords.length; i++) {
       targetAngles[i] = [];
-      for (j = 0; j < targetCoords[i].length; j++) {
-        targetAngles[i][j] = Math.atan2(targetCoords[i][j][1] - targetCoords[i][j][3], targetCoords[i][j][0] - targetCoords[i][j][2]);
+      for (j = 0; j < targetCoords[i].length - 1; j++) {
+        targetAngles[i][j] = Math.atan2(targetCoords[i][j + 1][1] - targetCoords[i][j][1], targetCoords[i][j + 1][0] - targetCoords[i][j][0]) * 180 / Math.PI;
       }
     }
     
