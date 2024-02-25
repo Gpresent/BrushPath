0767239bf8c698dc8ed08cffca7726597a250c48
Charlotte Harrington <c.harrington.0301@gmail.com>
2024-02-25 12:56:31 -0600

    android pwa build

---
+++ app/src/main/java/app/vercel/zenjilearn/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.zenjilearn.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
