6723b558a5e0ea0a11335d3b10a63030ae58542a
Charlotte Harrington <c.harrington.0301@gmail.com>
2024-02-28 13:41:51 -0600

    character type matches db

--- src/types/Character.ts
+++ src/types/Character.ts
@@ -1,8 +1,16 @@
 type Character = {
     id: number;
     unicode: string;
-    hiragana: string;
-    english: string;
+    on: string[] | null;
+    kun: string[] | null;
+    nanori: string[] | null;
+    radicals: any[] | null;
+    english: string[];
+    stroke_count: number;
+    freq: number | null;
+    grade: number | null;
+    jplt: number | null;
+    compounds: string[] | null;
 };
   
 export default Character;
