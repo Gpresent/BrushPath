c778e33838c23610a1efa881d8ad54728de5862f
torres-sam <torres.sam@tamu.edu>
2024-02-28 13:19:58 -0600

    Added skeleton for single word view

--- src/pages/SingleWord.tsx
+++ src/pages/SingleWord.tsx
@@ -1,1 +1,16 @@
-export {}
+
+interface SingleWordProps {
+    unicode: string;
+}
+
+const SingleWordView : React.FC<SingleWordProps> = ({unicode}) => {
+    return (
+      <div className="single-word-view">
+  
+        
+        
+      </div>
+    )
+  }
+
+export default SingleWordView;
