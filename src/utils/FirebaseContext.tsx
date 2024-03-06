71db696bd76077f5cc5d20b4e736b84ca7359184
torres-sam <torres.sam@tamu.edu>
2024-03-06 13:05:28 -0600

    Added firebase update on login

--- src/utils/FirebaseContext.tsx
+++ src/utils/FirebaseContext.tsx
@@ -6,6 +6,7 @@ import React, { ReactNode, createContext, useContext, useEffect, useState } from
 import Login from '../pages/Login';
 import Loading from '../components/Loading';
 import { GoogleAuthProvider } from "firebase/auth";
+import { Timestamp, doc, runTransaction } from 'firebase/firestore';
 
 
 //Initialize Context
@@ -27,6 +28,35 @@ export const AuthProvider = ({children}: { children:ReactNode}) => {
         user: user
     }
 
+    const updateUserDatabase = async (user:any) => {
+      try {
+        const userRef = await doc(db,"User",user.email);
+        await runTransaction(db, async (transaction) => {
+          
+          const userDoc = await transaction.get(userRef);
+          if (userDoc.exists()) {
+            throw "Document exists!";
+          }
+      
+          
+          transaction.set(userRef, {
+            admin: false,
+            student: false,
+            teacher: false,
+            last_login_time: Timestamp.now(),
+            decks:[],
+            name: user.displayName,
+            total_use_time: 0,
+          });
+          
+          //Add character_progress collection?
+        });
+        console.log("Transaction successfully committed!");
+      } catch (e) {
+        console.log("Transaction failed: ", e);
+      }
+    }
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((firebaseUser) => {
       console.log("auth state changed")
@@ -34,6 +64,9 @@ export const AuthProvider = ({children}: { children:ReactNode}) => {
       console.log(auth)
       setLoading(true);
       setUser(firebaseUser);
+      if(firebaseUser != null) {
+        updateUserDatabase(firebaseUser);
+      }
       setLoading(false);
     });
 
