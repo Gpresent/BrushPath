ca3c20d6174045d04f513c8b372a3c37e87289eb
gpresent <gpresent@tamu.edu>
2024-03-07 23:56:52 -0600

    Trying to remove ability to drag...

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ import ComponentRouter from './router/router';
 import { AuthProvider } from './utils/FirebaseContext';
  
 function App(this: any) {
+  document.addEventListener('dragstart', function(event) {
+    event.preventDefault();
+  });
+  
   return (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
