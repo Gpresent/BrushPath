6f1afaeca636143e72b0d939f2a1af74bf266e42
gpresent <gpresent@tamu.edu>
2024-03-20 10:24:18 -0500

    Making controller function for grading schema

--- src/grading/grade_svg.tsx
+++ src/grading/grade_svg.tsx
@@ -31,15 +31,13 @@ function gen_feedback_aspect(aspectScore: number): string {
     return '';
 }
 
-export default function grade_svg(input: string, targetKanji: string) {
+export default function grade_svg(input: string, targetKanji: string, passing: number = 0.6) {
         fetch("/interpolation_data/" + targetKanji.codePointAt(0)?.toString(16).padStart(5, '0') + ".json").then(response => response.json())
         .then(data => {
             var targetInfo = data as unknown as interp_data;
             const tCoords = targetInfo.coords;
             const iCoords = interpolate(input, targetInfo.totalLengths);
             if (!iCoords.length) return;
-
-            const passing = 0.6;
 
             const [inputCoords, targetCoords, aspectWarp] = fit_bbox(iCoords, tCoords);
             console.log(inputCoords, targetCoords)
@@ -57,39 +55,43 @@ export default function grade_svg(input: string, targetKanji: string) {
             const lengthDiffs = grade_lengths(inputCoords, targetCoords);
             const [centerDiffs, meanCenterDiffs] = grade_center_points(inputCoords, targetCoords);
             const [extraIntersections, missingIntersections] = grade_intersections(inputCoords, targetCoords);
-            console.log("Extra Intersections: ", extraIntersections);
-            console.log("Missing Intersections: ", missingIntersections);
+            //console.log("Extra Intersections: ", extraIntersections);
+            //console.log("Missing Intersections: ", missingIntersections);
             const [extraCrosses, missingCrosses] = grade_crosses(inputCoords, targetCoords);
-            console.log("Extra Crosses", extraCrosses);
-            console.log("Missing Crosses", missingCrosses);
+            //console.log("Extra Crosses", extraCrosses);
+            //console.log("Missing Crosses", missingCrosses);
 
             const grades: number[] = [];
+            const strokeInfo: string[] = [];
+            const feedback: string[] = [];
             for (let i = 0; i < inputCoords.length; i++) {
                 if (meanDiffs[i] > 90) meanDiffs[i] = 90;
 
                 const angleScore = (meanDiffs[i] / 70) / (Math.sqrt(squiggle[i]));
-                console.log("Squiggle factor: ", Math.sqrt(squiggle[i]));
+                //console.log("Squiggle factor: ", Math.sqrt(squiggle[i]));
                 const lengthScore = Math.min(Math.pow(lengthDiffs[i], 2) / (targetCoords[i].length * 10), 1);
                 const centerScore = Math.max((meanCenterDiffs[i] - 30) / 60, 0)
                 const intersectionScore = 
                     (extraIntersections[i].reduce((sum: number, e: number) => sum + (e > 0 ? 1 : 0), 0) + missingIntersections[i].reduce((sum: number, e: number) => sum + (e > 0 ? 1 : 0), 0)) * 0.3;
                 const crossScore = (extraCrosses[i].length + missingCrosses[i].length) * 0.3;
 
-                console.log("Stroke ", i + 1, " Angle Score: ", (1 - angleScore) * 100, "%, Length Score: ", ((1 - lengthScore) * 100).toPrecision(4), "%, Center Score: ", ((1 - centerScore) * 100).toPrecision(4), "%, Correct Intersections: ", intersectionScore === 0 ? "Yes" : "No", ", Correct Crosses: ", crossScore === 0 ? "Yes" : "No");
+                strokeInfo.push("Stroke ", (i + 1).toString(), " Angle Score: ", ((1 - angleScore) * 100).toString(), "%, Length Score: ", ((1 - lengthScore) * 100).toPrecision(4).toString(), "%, Center Score: ", ((1 - centerScore) * 100).toPrecision(4).toString(), "%, Correct Intersections: ", intersectionScore === 0 ? "Yes" : "No", ", Correct Crosses: ", crossScore === 0 ? "Yes" : "No");
 
                 grades[i] = Math.max(1 - intersectionScore - crossScore - angleScore - aspectScore - lengthScore - centerScore, 0);
 
                 if (grades[i] < passing) {
-                    var feedback = 'Stroke ' + (i + 1) + ':\n';
-                    if (angleScore > 1 - passing) feedback += gen_feedback_angles(angleDiffs[i]);
-                    if (lengthScore > 1 - passing) feedback += gen_feedback_lengths(lengthDiffs[i]);
-                    if (centerScore > 1 - passing) feedback += gen_feedback_center_points(centerDiffs[i]);
-                    feedback += gen_feedback_intersections(extraIntersections[i], missingIntersections[i]);
-                    feedback += gen_feedback_crosses(extraCrosses[i], missingCrosses[i]);
+                    var feedbackline = 'Stroke ' + (i + 1) + ':\n';
+                    if (angleScore > 1 - passing) feedbackline += gen_feedback_angles(angleDiffs[i]);
+                    if (lengthScore > 1 - passing) feedbackline += gen_feedback_lengths(lengthDiffs[i]);
+                    if (centerScore > 1 - passing) feedbackline += gen_feedback_center_points(centerDiffs[i]);
+                    feedbackline += gen_feedback_intersections(extraIntersections[i], missingIntersections[i]);
+                    feedbackline += gen_feedback_crosses(extraCrosses[i], missingCrosses[i]);
+                    feedbackline += gen_feedback_aspect(aspectScore);
+                    feedback[i] = feedbackline;
                 }
 
                 console.log("Grade: ", (grades[i] * 100).toPrecision(4), "%");
             }
-            color_input(grades, passing);
+            return [grades, strokeInfo, feedback];
     })
 }
