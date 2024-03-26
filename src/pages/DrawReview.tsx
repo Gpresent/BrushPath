eee58e82f7be52895a5deda78bb359d5690c1185
Charlotte Harrington <c.harrington.0301@gmail.com>
2024-03-26 16:41:48 -0500

    setup

---
+++ src/pages/DrawReview.tsx
@@ -0,0 +1,42 @@
+import Character from "../types/Character";
+import "../styles/styles.css";
+import "../styles/dict.css";
+import Draw from "./Draw";
+import { useLocation } from "react-router-dom";
+
+const DrawReview: React.FC = () => {
+  let { state } = useLocation();
+  let character : Character = state.character;
+
+  let meaning_len = 0;
+
+  let font_size = 28;
+
+  if (character && character.english) {
+    character.english.forEach((meaning) => {
+      meaning_len += meaning.length;
+    })
+  }
+
+  if(meaning_len > 30){
+    font_size = 20;
+    if(meaning_len > 75){
+      font_size = 15;
+    }
+  }
+
+  return (
+    <div className="draw-review">
+      {character && (
+        <>
+        <div className="character-prompt">
+            {character.one_word_meaning}
+        </div>
+          <Draw character={character} />
+          </>
+      )}
+    </div>
+  );
+};
+
+export default DrawReview;
