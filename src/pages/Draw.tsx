107225ac0684465783323f15cf5fca8b2406d0d6
Charlotte Harrington <c.harrington.0301@gmail.com>
2024-03-27 11:51:16 -0500

    review sketchpad outline

--- src/pages/Draw.tsx
+++ src/pages/Draw.tsx
@@ -2,26 +2,25 @@ import React, { useLayoutEffect, useRef } from "react";
 import "../styles/App.css";
 import { ReactSketchCanvas } from "react-sketch-canvas";
 import { useEffect } from "react";
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import ClearIcon from '@mui/icons-material/Clear';
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import ClearIcon from "@mui/icons-material/Clear";
 import color_input from "../grading/color_input";
 import grade_svg from "../grading/grade_svg";
-import '../styles/styles.css'
+import "../styles/styles.css";
 import Character from "../types/Character";
-
 
 const styles = {
   canvas: {
     position: "relative" as "relative",
-    width: '100%',
-    aspectRatio: '1/1',
-    maxWidth: '500px',
+    width: "100%",
+    aspectRatio: "1/1",
+    maxWidth: "500px",
     display: "flex",
-    border: '1px solid rgba(0, 0, 0, 1)',
-    borderRadius: '10px',
-    marginTop: '10px',
-    marginBottom: '10px',
+    border: "1px solid rgba(0, 0, 0, 1)",
+    borderRadius: "10px",
+    marginTop: "10px",
+    marginBottom: "10px",
     alignItems: "center",
     justifyContent: "center",
     zIndex: 2,
@@ -38,18 +37,18 @@ const styles = {
     // alignItems: "center",
     height: "100%",
     width: "100%",
-    maxWidth: "500px"
+    maxWidth: "500px",
   },
   svg: {
     position: "absolute" as "absolute",
     zIndex: -1,
-    opacity: .75,
+    opacity: 0.75,
   },
   gradeSvg: {
     position: "absolute" as "absolute",
     zIndex: 1,
     opacity: 1,
-  }
+  },
 
   // border: '1rem solid #9c9c9c',
   // borderRadius: '1rem',
@@ -59,28 +58,30 @@ const parser = new DOMParser();
 
 interface DrawProps {
   character?: Character;
+  allowDisplay: boolean;
 }
 
 const Draw: React.FC<DrawProps> = (props) => {
   const canvas: any = useRef<any>();
-  const [svgHtml, setSvgHtml] = React.useState({ __html: '' });
+  const [svgHtml, setSvgHtml] = React.useState({ __html: "" });
   const [displaySVG, setDisplaySVG] = React.useState<boolean>(false);
   const [readOnly, setReadOnly] = React.useState<boolean>(false);
   const [kanji, setKanji] = React.useState<string>("何");
   const [askInput, setAskInput] = React.useState<boolean>(true);
+  const [allowDisplaySVG, setAllowDisplaySVG] = React.useState<boolean>(props.allowDisplay);
 
-  useLayoutEffect(()=>{
-    if(props.character){
+  useLayoutEffect(() => {
+    if (props.character) {
       setKanji(props.character.unicode);
       setAskInput(false);
     }
-  })
+  });
 
   useEffect(() => {
     const loadSvg = async (unicode: string) => {
       // Load SVG dynamically
       try {
-        const svgModule = await fetch('/joyo_kanji/' + unicode + '.svg');
+        const svgModule = await fetch("/joyo_kanji/" + unicode + ".svg");
         var svgText = await svgModule.text();
         var doc = parser.parseFromString(svgText, "image/svg+xml");
         const svg = doc.getElementsByTagName("svg")[0];
@@ -100,40 +101,47 @@ const Draw: React.FC<DrawProps> = (props) => {
         svgText = svg.outerHTML;
 
         setSvgHtml({ __html: svgText });
-      } catch (e) {
-
-      }
+      } catch (e) {}
     };
     // const unicode = kanji?.codePointAt(0)?.toString(16).padStart(5, '0') || '';
-    const unicode = props.character?.unicode_str ? props.character?.unicode_str: "";
+    const unicode = props.character?.unicode_str
+      ? props.character?.unicode_str
+      : "";
     loadSvg(unicode);
   }, [kanji]);
 
   return (
     <div style={styles.container}>
       {askInput && (
-      <div className="kanji-input-wrapper">
-      <p className="kanji-input-prompt">Enter Kanji to Practice:</p>
-        <input
-          className="kanji-input-typed"
-          placeholder="Enter Kanji"
-          onChange={(e) => {
-            setKanji(e.target.value);
-          }}
-          value={kanji}
-        />
+        <div className="kanji-input-wrapper">
+          <p className="kanji-input-prompt">Enter Kanji to Practice:</p>
+          <input
+            className="kanji-input-typed"
+            placeholder="Enter Kanji"
+            onChange={(e) => {
+              setKanji(e.target.value);
+            }}
+            value={kanji}
+          />
         </div>
-        )}
+      )}
       <div style={styles.canvas}>
         <ReactSketchCanvas
           ref={canvas}
-          style={{width: '99%', height: '99%', borderRadius: '10px', pointerEvents: readOnly ? 'none' : 'auto'}}
+          style={{
+            width: "99%",
+            height: "99%",
+            borderRadius: "10px",
+            pointerEvents: readOnly ? "none" : "auto",
+          }}
           strokeWidth={7}
           strokeColor="rgba(40, 40, 41, .75)"
           canvasColor="rgba(214, 90, 181, 0.01)"
         />
-        {displaySVG && <div dangerouslySetInnerHTML={svgHtml} style={styles.svg} />}
-        <button 
+        {displaySVG && (
+          <div dangerouslySetInnerHTML={svgHtml} style={styles.svg} />
+        )}
+        <button
           className="clear-kanji"
           style={styles.button}
           onClick={() => {
@@ -143,30 +151,36 @@ const Draw: React.FC<DrawProps> = (props) => {
         >
           <ClearIcon></ClearIcon>
         </button>
-        <button
-          className="view-kanji"
-          style={styles.button}
-          onClick={() => {
-            setDisplaySVG(!displaySVG);
-          }}
-        >
-          {displaySVG ? <VisibilityOffIcon/> : <VisibilityIcon/>}
-        </button>
+        {allowDisplaySVG && (
+          <button
+            className="view-kanji"
+            style={styles.button}
+            onClick={() => {
+              setDisplaySVG(!displaySVG);
+            }}
+          >
+            {displaySVG ? <VisibilityOffIcon /> : <VisibilityIcon />}
+          </button>
+        )}
       </div>
       <button
         className="recolor-canvas"
         onClick={() => {
           setReadOnly(true);
-          canvas.current.exportSvg().then((data: any) => {
-          grade_svg(data, kanji);
-        }).catch((e: any) => {
-          console.log(e);
-        })}}
-        >
-          Check
-        </button>
+          canvas.current
+            .exportSvg()
+            .then((data: any) => {
+              grade_svg(data, kanji);
+            })
+            .catch((e: any) => {
+              console.log(e);
+            });
+        }}
+      >
+        Check
+      </button>
     </div>
   );
-}
+};
 
 export default Draw;
--- src/pages/DrawReview.tsx
+++ src/pages/DrawReview.tsx
@@ -1,12 +1,23 @@
 import Character from "../types/Character";
 import "../styles/styles.css";
+import "../styles/review.css";
 import "../styles/dict.css";
 import Draw from "./Draw";
 import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
-const DrawReview: React.FC = () => {
+interface DrawReviewProps {
+  setShowHeader: any;
+}
+
+const DrawReview: React.FC<DrawReviewProps> = ({ setShowHeader }) => {
+  // (document.getElementsByClassName('header-wrapper')[0]as any).style.visibility = 'none';
+  useEffect(() => {
+    setShowHeader(false);
+  }, [setShowHeader]);
+
   let { state } = useLocation();
-  let character : Character = state.character;
+  let character: Character = state.character;
 
   let meaning_len = 0;
 
@@ -15,12 +26,12 @@ const DrawReview: React.FC = () => {
   if (character && character.english) {
     character.english.forEach((meaning) => {
       meaning_len += meaning.length;
-    })
+    });
   }
 
-  if(meaning_len > 30){
+  if (meaning_len > 30) {
     font_size = 20;
-    if(meaning_len > 75){
+    if (meaning_len > 75) {
       font_size = 15;
     }
   }
@@ -29,11 +40,9 @@ const DrawReview: React.FC = () => {
     <div className="draw-review">
       {character && (
         <>
-        <div className="character-prompt">
-            {character.one_word_meaning}
-        </div>
-          <Draw character={character} />
-          </>
+          <div className="character-prompt">{character.one_word_meaning}</div>
+          <Draw character={character} allowDisplay={false} />
+        </>
       )}
     </div>
   );
