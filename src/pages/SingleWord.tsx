ff6b3a2a07f59d7e4b3a635c6fa421bb30e1e51f
Charlotte Harrington <c.harrington.0301@gmail.com>
2024-03-27 19:25:22 -0500

    back button

--- src/pages/SingleWord.tsx
+++ src/pages/SingleWord.tsx
@@ -2,11 +2,12 @@ import Character from "../types/Character";
 import "../styles/styles.css";
 import "../styles/dict.css";
 import Draw from "./Draw";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 
 const SingleWordView: React.FC = () => {
   let { state } = useLocation();
-  let character : Character = state.character;
+  let character: Character = state.character;
 
   let meaning_len = 0;
 
@@ -15,73 +16,86 @@ const SingleWordView: React.FC = () => {
   if (character && character.english) {
     character.english.forEach((meaning) => {
       meaning_len += meaning.length;
-    })
+    });
   }
 
-  if(meaning_len > 30){
+  if (meaning_len > 30) {
     font_size = 20;
-    if(meaning_len > 75){
+    if (meaning_len > 75) {
       font_size = 15;
     }
   }
-
+  let navigate = useNavigate();
   return (
-    <div className="single-word-view">
-      {character && (
-        <>
-          <div className="character-header">
-            <div className="character-kanji">{character.unicode}</div>
-            {character.english.length > 0 && (
-            <div className="character-meanings" style={{fontSize: `${font_size}px`}}>
-              {character.english.map((meaning, index) => {
-                return <span>{(index ? ", " : "") + meaning}</span>;
-              })}
+    <>
+      <div className="single-word-view">
+        {character && (
+          <>
+            <div className="character-header">
+              <div className="character-kanji">
+                <div
+                  style={{ display: "flex", alignItems: "center" }}
+                  onClick={() => navigate(-1)}
+                >
+                  <ArrowBackIosNewIcon></ArrowBackIosNewIcon>
+                </div>
+                {character.unicode}
+              </div>
+              {character.english.length > 0 && (
+                <div
+                  className="character-meanings"
+                  style={{ fontSize: `${font_size}px` }}
+                >
+                  {character.english.map((meaning, index) => {
+                    return <span>{(index ? ", " : "") + meaning}</span>;
+                  })}
+                </div>
+              )}
             </div>
-            )}
-          </div>
-          <div className="character-info">
-            <div className="info-line">stroke: {character.stroke_count} </div>
-            {character.kun.length > 0 && (
-              <div className="info-line">
-                kun:{" "}
-                {character.kun.map((kun, index) => {
-                  return (index ? ", " : "") + kun;
-                })}
-              </div>
-            )}
-            {character.on.length > 0 && (
-              <div className="info-line">
-                on:{" "}
-                {character.on.map((on, index) => {
-                  return (index ? ", " : "") + on;
-                })}
-              </div>
-            )}
-            {character.radicals.length > 0 && (
-              <div className="info-line">
-                radicals:{" "}
-                {character.radicals.map((radical, index) => {
-                  return (index ? ", " : "") + radical;
-                })}
-              </div>
-            )}
-            {character.jlpt && (
-              <div className="info-line">JLPT: {character.jlpt} </div>
-            )}
-            {character.freq && (
-              <div className="info-line">frequency: {character.freq} </div>
-            )}
-            {character.grade && (
-              <div className="info-line">grade: {character.grade} </div>
-            )}
-          </div>
-          <div className="mastery-desc">
-            You are <b>unfamiliar</b> with this character.
-          </div>
-          <Draw character={character} allowDisplay={true} />
-        </>
-      )}
-    </div>
+            <div className="character-info">
+              <div className="info-line">stroke: {character.stroke_count} </div>
+              {character.kun.length > 0 && (
+                <div className="info-line">
+                  kun:{" "}
+                  {character.kun.map((kun, index) => {
+                    return (index ? ", " : "") + kun;
+                  })}
+                </div>
+              )}
+              {character.on.length > 0 && (
+                <div className="info-line">
+                  on:{" "}
+                  {character.on.map((on, index) => {
+                    return (index ? ", " : "") + on;
+                  })}
+                </div>
+              )}
+              {character.radicals.length > 0 && (
+                <div className="info-line">
+                  radicals:{" "}
+                  {character.radicals.map((radical, index) => {
+                    return (index ? ", " : "") + radical;
+                  })}
+                </div>
+              )}
+              {character.jlpt && (
+                <div className="info-line">JLPT: {character.jlpt} </div>
+              )}
+              {character.freq && (
+                <div className="info-line">frequency: {character.freq} </div>
+              )}
+              {character.grade && (
+                <div className="info-line">grade: {character.grade} </div>
+              )}
+            </div>
+            <div className="mastery-desc">
+              You are <b>unfamiliar</b> with this character.
+            </div>
+            <Draw character={character} allowDisplay={true} />
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import DictionaryView from '../pages/Dictionary';
 import Home from '../pages/Home';
 import Draw from '../pages/Draw';
