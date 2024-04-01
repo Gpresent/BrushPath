bfd590379a6dfee9fe55ae53c188df99d62dd13a
gpresent <gpresent@tamu.edu>
2024-04-01 09:28:54 -0500

    Added KanjiGrade type

---
+++ src/types/KanjiGrade.ts
@@ -0,0 +1,9 @@
+type KanjiGrade = {
+    overallGrade: number;
+    overallFeedback: string;
+    grades: number[];
+    feedback: string[];
+    strokeInfo: string[];
+}
+
+export default KanjiGrade;
