edfb83cd7b49f0dd98283b745bb7ddc7060716a8
Liam Benkel <liambenkel@tamu.edu>
2024-04-02 14:21:42 -0500

    added DarkModeContext

---
+++ src/components/DarkModeContext.tsx
@@ -0,0 +1,38 @@
+// DarkModeContext.tsx
+import React, { createContext, useState, useEffect, useContext} from 'react';
+
+interface DarkModeContextProps {
+  isDarkMode: boolean;
+  toggleDarkMode: () => void;
+}
+
+const defaultState = {
+  isDarkMode: false,
+  toggleDarkMode: () => {}
+};
+
+const DarkModeContext = createContext<DarkModeContextProps>(defaultState);
+
+export const useDarkMode = () => useContext(DarkModeContext);
+
+export const DarkModeProvider: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(() => {
+    const savedMode = localStorage.getItem('darkMode');
+    return savedMode !== null ? JSON.parse(savedMode) : false;
+  });
+
+  useEffect(() => {
+    document.body.classList.toggle('dark-mode', isDarkMode);
+    localStorage.setItem('darkMode', JSON.stringify(isDarkMode));
+  }, [isDarkMode]);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,32 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { auth } from '../utils/Firebase';
 import { signOut } from 'firebase/auth';
 import "../styles/settings.css";
 import { googleLogout } from "@react-oauth/google";
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import UsernameModal from '../components/UsernameModal';
-
+import { useDarkMode } from '../components/DarkModeContext';
+import PrivacyPolicyModal from '../components/PrivacyPolicyModal';
 
 const SettingsView: React.FC = () => {
-  const [isDarkMode, setIsDarkMode] = useState(() => {
-    return JSON.parse(localStorage.getItem('darkMode') || 'false');
-  });
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
   const [showUsernameModal, setShowUsernameModal] = useState(false);
-
-
-  useEffect(() => {
-    if (isDarkMode) {
-      document.body.classList.add('dark-mode');
-    } else {
-      document.body.classList.remove('dark-mode');
-    }
-
-    localStorage.setItem('darkMode', JSON.stringify(isDarkMode));
-  }, [isDarkMode]);
-
-  const handleToggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode); // This should toggle the isDarkMode state
-  };
+  const [showPrivacyPolicyModal, setShowPrivacyPolicyModal] = useState(false);
 
   const handleArrowClick = (spanName: string) => {
     // Perform different actions based on spanName
@@ -41,7 +26,7 @@ const SettingsView: React.FC = () => {
         console.log("Accessibility Arrow Clicked")
         break;
       case 'Privacy Policy':
-        console.log("Privacy Policy Arrow Clicked")
+        setShowPrivacyPolicyModal(true);
         break;
       case 'Terms of Service':
         console.log("Terms of Service Arrow Clicked")
@@ -51,6 +36,11 @@ const SettingsView: React.FC = () => {
         console.log("Nothing to see here people 0_0")
         break;
     }
+  };
+
+  const closePrivacyPolicyModal = () => {
+
+    setShowPrivacyPolicyModal(false);
   };
 
   const closeUsernameModal = () => {
@@ -72,10 +62,10 @@ const SettingsView: React.FC = () => {
             <span onClick={() => handleArrowClick('Username')}>Username</span>
             <ArrowForwardIcon className="right-arrow" onClick={() => handleArrowClick('Username')} />
           </div>
-          <div className="settings-item">
+          {/* <div className="settings-item">
             <span onClick={() => handleArrowClick('Profile Picture')}>Profile Picture</span>
             <ArrowForwardIcon className="right-arrow" onClick={() => handleArrowClick('Profile Picture')} />
-          </div>
+          </div> */}
         </div>
 
         <p className="title">Preferences</p>
@@ -85,13 +75,13 @@ const SettingsView: React.FC = () => {
             <div
               id="toggleButton"
               className={`toggle-button ${isDarkMode ? 'active' : ''}`} //if true/false, shorthand goes crazy
-              onClick={handleToggleDarkMode}
+              onClick={toggleDarkMode}
             ></div>
           </div>
-          <div className="settings-item">
+          {/* <div className="settings-item">
             <span onClick={() => handleArrowClick('Accessibility')}>Accessibility</span>
             <ArrowForwardIcon className="right-arrow" onClick={() => handleArrowClick('Accessibility')} />
-          </div>
+          </div> */}
         </div>
 
         <p className="title">About</p>
@@ -100,14 +90,14 @@ const SettingsView: React.FC = () => {
             <span onClick={() => handleArrowClick('Privacy Policy')}>Privacy Policy</span>
             <ArrowForwardIcon className="right-arrow" onClick={() => handleArrowClick('Privacy Policy')} />
           </div>
-          <div className="settings-item">
+          {/* <div className="settings-item">
             <span onClick={() => handleArrowClick('Terms of Service')}>Terms of Service</span>
             <ArrowForwardIcon className="right-arrow" onClick={() => handleArrowClick('Terms of Service')} />
-          </div>
-          <div className="settings-item">
+          </div> */}
+          {/* <div className="settings-item">
             <span>Contact Us</span>
             <ArrowForwardIcon className="right-arrow"></ArrowForwardIcon>
-          </div>
+          </div> */}
 
         </div >
       </div >
@@ -126,6 +116,11 @@ const SettingsView: React.FC = () => {
         isOpen={showUsernameModal}
         onClose={closeUsernameModal}
         onSubmit={handleUsernameSubmit}
+      />
+
+      <PrivacyPolicyModal 
+        isOpen={showPrivacyPolicyModal}
+        onClose={closePrivacyPolicyModal}
       />
     </div >
   );
