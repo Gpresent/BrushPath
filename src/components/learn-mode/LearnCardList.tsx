189d91cf12217297c31ff72e656fc73500f51575
torres-sam <torres.sam@tamu.edu>
2024-04-08 19:53:53 -0500

    Added basic styling for Learning flashcard

---
+++ src/components/learn-mode/LearnCardList.tsx
@@ -0,0 +1,69 @@
+import react, { useEffect, useMemo, useRef, useState } from 'react';
+import Character from '../../types/Character';
+import Draw from '../../pages/Draw';
+import '../../styles/learn.css'
+
+interface LearnCardListProps {
+    characters: Character[];
+    
+}
+type CharacterSessionData = {
+    score?: number;
+
+}
+
+const LearnCardList: React.FC<LearnCardListProps>  = ({characters}) => {
+
+    const [currentCharacterIndex, setCurrentCharacterIndex] = useState<number>(0);
+
+    const [characterSessionData, setCharacterSessionData] = useState<(Character & CharacterSessionData)[]>(characters);
+
+    const canvasRef = useRef<any>();
+
+    const currentCharacter = useMemo(() => {
+        return characterSessionData[currentCharacterIndex]
+    },[currentCharacterIndex])
+    useEffect(() => {
+
+    },[]);
+
+    //Todo, replace divs with actual tags lol
+    return (
+        <div className='learn-cards-container'>
+        <div className="learn-card">
+            <div className="learn-card-title">
+                {currentCharacter.one_word_meaning}
+            </div>
+            <div className="learn-card-info">
+                
+            </div>
+            <div className="learn-card-draw-container" >
+                <Draw allowDisplay={false} forwardRef={canvasRef} character={currentCharacter}  />
+            </div>  
+            
+            <div className="learn-card-nav-row">
+            <button onClick={() => {
+                //Check if at 0
+                if(currentCharacterIndex > 0) {
+                    setCurrentCharacterIndex((prevIndex) => prevIndex - 1);
+                }
+            }} className="learn-card-nav-left">
+                {'<'}
+            </button>
+            <button onClick={() => {
+                if(currentCharacterIndex < characterSessionData.length-1) {
+                    setCurrentCharacterIndex((prevIndex) => prevIndex + 1);
+                }
+            }}className="learn-card-nav-right">
+                {'>'}
+            </button>
+            
+            </div>
+        </div>
+        </div>
+    )
+}
+
+
+
+export default LearnCardList;
--- src/pages/LearnDeck.tsx
+++ src/pages/LearnDeck.tsx
@@ -4,13 +4,16 @@ import { getCharacterScoreData, getCharsFromRefs, getDeckFromID, getHydratedChar
 import { DocumentData, DocumentReference } from "firebase/firestore";
 import { AuthContext } from "../utils/FirebaseContext";
 import LoadingSpinner from "../components/LoadingSpinner";
+import LearnCardList from "../components/learn-mode/LearnCardList";
+import characterParser from "../utils/characterParser";
+import Character from "../types/Character";
 
 
 interface LearnProps  {
     
 };
 type RetrievableData = {
-    data: DocumentData | null;
+    data: Character[] | null;
     loading: boolean;
     error: string;
   };
@@ -19,11 +22,7 @@ type RetrievableData = {
 
 const LearnDeck: React.FC<LearnProps> = ({  }) => {
 
-    const [characterRefs, setCharacterRefs] = useState<RetrievableData>({
-        data: null,
-        loading: true,
-        error: "",
-      });
+    
       const [characters, setCharacters] = useState<RetrievableData>({
         data: null,
         loading: true,
@@ -36,63 +35,70 @@ const LearnDeck: React.FC<LearnProps> = ({  }) => {
 
   useEffect(() => {
     
-    if(!id) {
-        setCharacterRefs({ data: null, loading: false, error: "No url parameter" });
-        return;
+    if(!userData) {
+        getUserData();
     }
-    if(!user?.email) {
-        setCharacterRefs({ data: null, loading: false, error: "No email" });
-        return;
-    }
-    const promises = Promise.all([getDeckFromID(id), getCharacterScoreData(user?.email)]);
-    promises.then(([deck ,score]) => {
-        let deckData = deck as {_id:string} &DocumentData
-        let scoreData = score as DocumentData[] | null
-        console.log(deckData)
-        console.log(scoreData)
-        if(!deckData || !deckData?.characters) {
-            setCharacterRefs({ data: null, loading: false, error: "Deck data not found" });
-            return;
-        }
-        if(scoreData === null || scoreData === undefined) {
-            setCharacterRefs({ data: null, loading: false, error: "Error getting character score" });
-            return;
-        }
-        //Filter out learned characters
-        let potentialCharacters: DocumentReference[] = [];
-        deckData.characters.forEach((character: DocumentReference) => {
-            if(!scoreData?.some(char => char.characterRef === character.id)) {
-                potentialCharacters.push(character)
-            }
-        });
-
-        setCharacterRefs({ data: potentialCharacters.slice(0,30), loading: false, error: "" });
-        
-        getCharsFromRefs(potentialCharacters, 0).then((chars) => {
-            if(!chars) {
-                setCharacters({ data: null, loading: false, error: "Error fetching character data" });
-                return;
-            }
-            setCharacters({ data: chars, loading: false, error: "" });
-            
-        })
-        
-    })
     
     
     
   }, []);
+
+  useEffect(() => {
+    if(userData) {
+        if(!id) {
+            setCharacters({ data: null, loading: false, error: "No url parameter" });
+            return;
+        }
+        if(!userData?.email) {
+            setCharacters({ data: null, loading: false, error: "No email" });
+            return;
+        }
+        const promises = Promise.all([getDeckFromID(id), getCharacterScoreData(userData?.email)]);
+        promises.then(([deck ,score]) => {
+            let deckData = deck as {_id:string} &DocumentData
+            let scoreData = score as DocumentData[] | null
+            console.log(deckData)
+            console.log(scoreData)
+            if(!deckData || !deckData?.characters) {
+                setCharacters({ data: null, loading: false, error: "Deck data not found" });
+                return;
+            }
+            if(scoreData === null || scoreData === undefined) {
+                setCharacters({ data: null, loading: false, error: "Error getting character score" });
+                return;
+            }
+            //Filter out learned characters
+            let potentialCharacters: DocumentReference[] = [];
+            deckData.characters.forEach((character: DocumentReference) => {
+                
+                if(!scoreData?.some(char => char.characterRef.id === character.id)) {
+                   
+                    potentialCharacters.push(character)
+                }
+            });
+    
+            // setCharacters({ data: potentialCharacters.slice(0,30), loading: false, error: "" });
+            
+            getCharsFromRefs(potentialCharacters, 0).then((fetchedChars) => {
+                if(!fetchedChars) {
+                    setCharacters({ data: null, loading: false, error: "Error fetching character data" });
+                    return;
+                }
+                setCharacters({ data: fetchedChars
+                    .map((character: any) => characterParser(character)).filter((character: Character | null): character is Character => character !== null), loading: false, error: "" });
+                
+            })
+            
+        })
+    }
+  },[userData])
     
 
     return (<div>
         {characters.loading? <LoadingSpinner /> :
         characters.error || characters.data == null? <p>{characters.error}</p>:
-            characters.data.map((char: any) => {
-                return (<p key={char.unicode}>
-                    {JSON.stringify(char)}
-                </p>)
-            })
-        }) 
+        <LearnCardList characters={characters.data} />
+        }
         
     </div>)
 }
