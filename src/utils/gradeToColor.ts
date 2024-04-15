e7ace8ff39e4e818be4e848df6022c7916980620
gpresent <gpresent@tamu.edu>
2024-04-15 13:51:51 -0500

    Altered grade ranges in gradeToColor to reflect passing of 65

--- src/utils/gradeToColor.ts
+++ src/utils/gradeToColor.ts
@@ -6,11 +6,11 @@ export default function gradeToColor(score: number) {
 
     if (0 <= score && score < 40){
         return "#D9544D";
-    }else if(40 < score && score < 60){
+    }else if(45 < score && score < 65){
         return "#F69B46";
-    }else if(60 < score && score < 80){
+    }else if(65 < score && score < 85){
         return "#FFFBB9";
-    }else if(80 < score){
+    }else if(85 < score){
         return "#8EF076";
     }else{
         return "rgba(0,0,0,0)";
@@ -22,13 +22,13 @@ export function gradeToWord(score: number) {
 
     // console.log("score is: " + score)
 
-    if (0 <= score && score < 40){
+    if (0 <= score && score < 45){
         return "Try Again";
-    }else if(40 < score && score < 60){
+    }else if(45 < score && score < 65){
         return "Needs Work";
-    }else if(60 < score && score < 80){
+    }else if(65 < score && score < 85){
         return "Fair";
-    }else if(80 < score && score < 100){
+    }else if(85 < score && score < 100){
         return "Good";
     }else if (score === 100){
         return "Perfect";
