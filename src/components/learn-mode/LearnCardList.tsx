466b7a9544fd80fbd066a1296616fbbabca7a1d7
Charlotte Harrington <c.harrington.0301@gmail.com>
2024-04-17 14:50:22 -0500

    learn is in modal now

--- src/components/learn-mode/LearnCardList.tsx
+++ src/components/learn-mode/LearnCardList.tsx
@@ -8,11 +8,12 @@ import { grad } from "@tensorflow/tfjs";
 // import { CharacterSearchProvider } from '../../utils/FirebaseCharacterSearchContext';
 import { useNavigate, useParams } from "react-router-dom";
 import WideModal from "../WideModal";
+import LoadingBar from "../LoadingBar";
 
 interface LearnCardListProps {
   characters: Character[];
   refetch?: (numCharacters?: number) => void;
-  learn:boolean
+  learn: boolean;
 }
 type CharacterSessionData = Character & {
   score?: KanjiGrade;
@@ -21,7 +22,7 @@ type CharacterSessionData = Character & {
 const LearnCardList: React.FC<LearnCardListProps> = ({
   characters,
   refetch,
-  learn
+  learn,
 }) => {
   const [currentCharacterIndex, setCurrentCharacterIndex] = useState<number>(0);
 
@@ -31,6 +32,8 @@ const LearnCardList: React.FC<LearnCardListProps> = ({
   const [finished, setFinished] = useState<boolean>(false);
 
   const [showSessionModal, setShowSessionModal] = useState<boolean>(false);
+  const [showLearnModal, setShowLearnModal] = useState<boolean>(true);
+
 
   const canvasRef = useRef<any>();
 
@@ -89,7 +92,13 @@ const LearnCardList: React.FC<LearnCardListProps> = ({
 
   function onClose() {
     navigate(`/deck/${id}`);
-    setShowSessionModal(false)
+    setShowSessionModal(false);
+  }
+
+  function onCloseLearn() {
+    setFinished(true);
+    navigate(`/deck/${id}`);
+    setShowLearnModal(false);
   }
 
   const navigate = useNavigate();
@@ -116,38 +125,68 @@ const LearnCardList: React.FC<LearnCardListProps> = ({
             <div className="session-info-header">Here's what you did:</div>
             <div className="session-info">
               <div>Learned {currentCharacterIndex} new characters: </div>
-              <div >
-              {characterSessionData
-                .filter((character) => {
-                  return character.score !== undefined;
-                })
-                .map((word, index) => {
-                  return (index ? ", " : "") + word.unicode + " (" + word.one_word_meaning + ")";
-                })}
-                </div>
+              <div>
+                {characterSessionData
+                  .filter((character) => {
+                    return character.score !== undefined;
+                  })
+                  .map((word, index) => {
+                    return (
+                      (index ? ", " : "") +
+                      word.unicode +
+                      " (" +
+                      word.one_word_meaning +
+                      ")"
+                    );
+                  })}
+              </div>
               <p>Average Score: {Math.floor(avgScore * 100) || 0}</p>
             </div>
             <div className="session-buttons">
-            <button
-              onClick={() => {
-                navigate(`/`);
-              }}
-            >
-              Return to Home
-            </button>
-            {/* Or
+              <button
+                onClick={() => {
+                  navigate(`/`);
+                }}
+              >
+                Return to Home
+              </button>
+              {/* Or
             <button onClick={() => {}}>New Study Session</button> */}
             </div>
           </div>
         </WideModal>
       ) : (
+        <WideModal
+          title={"Learn Session"}
+          isOpen={showLearnModal}
+          onClose={onCloseLearn}
+        >
         <div className="learn-container">
-          <LearnCard character={currentCharacter} learn={learn} handleAdvance={handleAdvance} />
-          <div className="learn-card-nav-row">
-          <p style={{marginTop:"7.5px"}}>Kanji Completed: {currentCharacterIndex}/{characterSessionData.length}</p>
-          <button onClick={()=>{console.log("Session Done"); setFinished(true); setShowSessionModal(true);}}>End Session</button>
+        <LoadingBar progress={currentCharacterIndex} duration={characterSessionData.length} message={""}></LoadingBar>
+
+        <LearnCard
+            character={currentCharacter}
+            learn={learn}
+            handleAdvance={handleAdvance}
+          />
+          {/* <div className="learn-card-nav-row">
+            <p style={{ marginTop: "7.5px" }}>
+              Kanji Completed: {currentCharacterIndex}/
+              {characterSessionData.length}
+            </p>
+            <button
+              onClick={() => {
+                console.log("Session Done");
+                setFinished(true);
+                setShowSessionModal(true);
+              }}
+            >
+              End Session
+            </button>
+          </div> */}
+
         </div>
-        </div>
+        </WideModal>
       )}
     </>
   );
